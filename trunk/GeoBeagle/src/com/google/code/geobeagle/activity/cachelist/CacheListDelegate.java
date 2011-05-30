038387d3d155f16feb3d10cabc4f744d05389bbd
Stephen Ng <stephen@theleengs.com>
2011-05-29 17:15:13 -0700

    Extract ImportIntentManager.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListDelegate.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListDelegate.java
@@ -38,38 +38,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class CacheListDelegate {
-    static class ImportIntentManager {
-        static final String INTENT_EXTRA_IMPORT_TRIGGERED = "com.google.code.geabeagle.import_triggered";
-        private final Activity mActivity;
-
-        @Inject
-        ImportIntentManager(Activity activity) {
-            this.mActivity = activity;
-        }
-
-        boolean isImport() {
-            Intent intent = mActivity.getIntent();
-            if (intent == null)
-                return false;
-
-            String action = intent.getAction();
-            if (action == null)
-                return false;
-
-            if (!action.equals("android.intent.action.VIEW"))
-                return false;
-
-            if (intent.getBooleanExtra(INTENT_EXTRA_IMPORT_TRIGGERED, false))
-                return false;
-
-            // Need to alter the intent so that the import isn't retriggered if
-            // pause/resume is a result of the phone going to sleep and then
-            // waking up again.
-            intent.putExtra(INTENT_EXTRA_IMPORT_TRIGGERED, true);
-            return true;
-        }
-    }
-
     private final ActivitySaver activitySaver;
     private final ActivityVisible activityVisible;
     private final CacheListRefresh cacheListRefresh;
